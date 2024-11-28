86f8e792a2dd2d71b5086ece8a60272552027252
Марина Васильева <marina_paley@mail.ru>
2024-11-28 18:35:14 +0300

    изменения

--- Функции и процедуры 2/sum_and_product_with_tab.sql
+++ Функции и процедуры 2/sum_and_product_with_tab.sql
@@ -4,13 +4,15 @@ INSERT INTO tab
 VALUES (5);
 
 --DROP FUNCTION sum_and_product_with_tab(x int);
-CREATE FUNCTION sum_and_product_with_tab (x int)
+CREATE OR REPLACE FUNCTION sum_and_product_with_tab (x int)
 RETURNS TABLE
 (sum int, product int)
 AS $$
-    SELECT $1 + tab.y
-         , $1 * tab.y
+    SELECT x + tab.y
+         , x * tab.y
     FROM tab;
 $$ LANGUAGE SQL;
 
 SELECT sum_and_product_with_tab(3);
+
+DELETE FROM tab;
